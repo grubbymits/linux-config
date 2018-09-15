ab7b8123b9fa3ab690e6a129e9da8165a9c24b16
Sam Parker <grubbymitts69@gmail.com>
2018-09-15 09:02:21 +0000

    commented out install of llvm

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# Use lsb-release to determine whether we're on ubuntu or debian
+#sudo apt-get update && sudo apt-get upgrade && sudo apt-get install lsb-release
+
+# Add the LLVM repos
+#sudo echo "deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch-7 main" > /etc/apt/sources.list
+#sudo echo "deb-src http://apt.llvm.org/stretch/ llvm-toolchain-stretch-7 main" > /etc/apt/sources.list
+#wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+
+sudo apt-get update && sudo apt-get upgrade && \
+sudo apt-get install vim htop tmux byobu build-essential
+
 mkdir -p ~/.vim/colors
 wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 mv solarized.vim ~/.vim/colors
+
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
