eb7eae88acecd1895cabe20efc4a7e9fd3687cda
Samuel Parker-Haynes <grubbymitts69@gmail.com>
2020-05-11 10:15:32 +0100

    changes for typescript

--- setup.sh
+++ setup.sh
@@ -9,12 +9,18 @@
 #wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 
 sudo apt-get update && sudo apt-get upgrade && \
-sudo apt-get install vim htop tmux byobu build-essential
+sudo apt-get install vim htop tmux byobu build-essential cmake nodejs npm
 
+git config --global user.email "grubbymitts69@gmail.com"
+git config --global user.name "Samuel Parker-Haynes"
+
+# Setup vim with solarized colours and syntax highlighting for typescript.
 mkdir -p ~/.vim/colors
 mkdir ~/.vim/syntax
 wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 mv solarized.vim ~/.vim/colors
+mkdir -p ~/.vim/pack/typescript/start/
+git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/pack/typescript/start/typescript-vim
 
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
